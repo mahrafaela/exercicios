e6cd0623d99c415a028f6395cc656cac2364de84
mahrafaela <128821670+mahrafaela@users.noreply.github.com>
2024-02-28 09:15:37 -0300

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,19 @@
+#contador
+contador=5 
+
+while(contador >0):
+    print('contador') 
+    contador= contador-1
+    
+print('fim')
+
+
+
+
+
+
+
+
+
+
+
---
+++ tabuada.py
@@ -0,0 +1,23 @@
+#tabuada
+numero= int(input('digite um numero:'))
+multiplicador=1
+
+while(multiplicador<=10):
+    resultado= numero * multiplicador
+
+    print(f'{numero} x {multiplicador} = {resultado}')
+
+    multiplicador= multiplicador+1 
+
+
+
+
+
+
+
+
+
+
+
+
+
