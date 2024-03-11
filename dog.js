ec8b173126ff12e9a6938a0b5e75ca06362c2ac5
mahrafaela <128821670+mahrafaela@users.noreply.github.com>
2024-03-11 09:03:37 -0300

    Add files via upload

---
+++ dog.js
@@ -0,0 +1,11 @@
+//Variáveis representando idade e cidadania
+let idade = 25;
+let possuiCarteiraDeMotorista = true;
+
+//Verificar se a idade é maior ou igual a 18 e se a cidadania é brasileira (true)
+
+if(idade >= 18 && possuiCarteiraDeMotorista) {console.log ("pode dirigir");
+} else { 
+    console.log("não pode dirigir");
+}
+
